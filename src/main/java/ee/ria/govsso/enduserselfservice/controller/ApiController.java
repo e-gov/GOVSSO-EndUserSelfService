577455fd437c628038025b6bd02704111e5a90e4
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2023-05-11 16:10:23 +0300

    GSSO-617 Use REST endpoints for ending sessions instead of redirects

--- src/main/java/ee/ria/govsso/enduserselfservice/controller/ApiController.java
+++ src/main/java/ee/ria/govsso/enduserselfservice/controller/ApiController.java
@@ -6,39 +6,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 
-// TODO: Return HTTP code instead of redirect
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ApiController {
 
     private final GovssoSessionService govssoSessionService;
 
-    @GetMapping("/sessions/{sessionId}") // TODO: DELETE
+    @DeleteMapping("/sessions/{sessionId}")
     public Mono<?> endSession(@AuthenticationPrincipal OidcUser oidcUser, @PathVariable String sessionId) {
         if (oidcUser == null) {
             return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
         }
         return govssoSessionService.endSession(oidcUser.getSubject(), sessionId)
-                .then(Mono.just(new RedirectView("/")));
+                .then(Mono.just(ResponseEntity.status(HttpStatus.OK).build()));
     }
 
-    @GetMapping("/sessions") // TODO: DELETE
+    @DeleteMapping("/sessions")
     public Mono<?> endAllSessions(@AuthenticationPrincipal OidcUser oidcUser) {
         if (oidcUser == null) {
-            return Mono.just(new ModelAndView("index"));
+            return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
         }
         return govssoSessionService.endSubjectSessions(oidcUser.getSubject())
-                .then(Mono.just(new RedirectView("/")));
+                .then(Mono.just(ResponseEntity.status(HttpStatus.OK).build()));
     }
 
 }
